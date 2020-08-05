0bf398ffa649ffb97fb12279c1b04b6e93641d8f
aditya gupta <adityasgupta309@gmail.com>
2020-08-05 19:30:01 +0530

    zxcv

--- file.js
+++ file.js
@@ -1,15 +1,12 @@
 checking git file...
 in repos
-<<<<<<< Updated upstream
-=======
+
 qwererw
 qwertyuiop
 asdfghjkl
 only qwerty
 qwer
 looking.................
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
+
 wrinting something to chk
->>>>>>> Stashed changes
+1234567890
